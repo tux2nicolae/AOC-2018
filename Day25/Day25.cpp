8b8e28803c32b5225c08a39f3076a99526b81a84
Telechi Nicolae <39991250+tux2nicolae@users.noreply.github.com>
2018-12-25 11:02:28 +0200

    Add day 25 solution

---
+++ Day25/Day25.cpp
@@ -0,0 +1,130 @@
+﻿/**
+ * Advent of code 2018
+ * @author : Nicolae Telechi
+ */
+#include <iostream>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <map>
+#include <unordered_map>
+#include <assert.h>
+#include <queue>
+#include <stack>
+#include <numeric>
+#include <set>
+#include <regex>
+
+using namespace std;
+
+int Abs2(const int x)
+{
+  if (x > 0)
+    return x;
+
+  return -x;
+};
+
+//----------------------------------------------------------------------------
+
+struct Point
+{
+  int id{};
+
+  int x{};
+  int y{};
+  int z{};
+  int t{};
+
+  int neighbours{};
+};
+
+int Distance(const Point & first, const Point & second)
+{
+  return Abs2(first.x - second.x) + Abs2(first.y - second.y) + Abs2(first.z - second.z) + Abs2(first.t - second.t);
+};
+
+vector<Point> stars;
+
+bool intersects[1500][1500];
+bool visited[1500];
+
+void dfs(int currentNode) 
+{
+  visited[currentNode] = true;
+
+  for (auto & star : stars)
+  {
+    if (intersects[currentNode][star.id] && !visited[star.id])
+    {
+      visited[star.id] = true;
+      dfs(star.id);
+    }
+  }
+}
+
+int GetConexComponents() {
+  int components = 0;
+  for (auto & star : stars)
+  {
+    if (!visited[star.id])
+    {
+      dfs(star.id);
+      components++;
+    }
+  }
+
+  return components;
+}
+
+int main()
+{
+  ifstream in("C:\\Users\\Telechi\\Desktop\\Day25.in");
+  ofstream out("C:\\Users\\Telechi\\Desktop\\Day25.out");
+  assert(in.good());
+  assert(out.good());
+
+  int id = 0;
+
+  // read data
+  string line;
+  while (getline(in, line))
+  {
+    smatch match;
+    regex_match(line, match, regex("(.*),(.*),(.*),(.*)"));
+    assert(match.size() == 5);
+
+    Point star;
+    star.id = id++;
+    star.x = stoi(match[1]);
+    star.y = stoi(match[2]);
+    star.z = stoi(match[3]);
+    star.t = stoi(match[4]);
+
+    stars.push_back(star);
+
+  }
+
+  // calculate intersections
+  for (auto & currentStar : stars)
+  {
+    int intersections = 0;
+    for (auto & neighbourStar : stars)
+    {
+      auto d = Distance(currentStar, neighbourStar);
+      if (d <= 3)
+      {
+        intersections++;
+        intersects[currentStar.id][neighbourStar.id] = true;
+      }
+    }
+
+    currentStar.neighbours = intersections;
+  }
+
+  cout << GetConexComponents();
+  return 0;
+}
